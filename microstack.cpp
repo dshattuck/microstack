2c686f36f0f82f42c2c93a8cbb66bf248c4d4e73
dshattuck <david.shattuck@gmail.com>
2026-03-04 09:08:45 -0800

    added slab writing and variable chunk size; improved error handling

--- microstack.cpp
+++ microstack.cpp
@@ -4,7 +4,8 @@
 // https://spdx.org/licenses/LGPL-2.1-only.html
 
 #include <argparser.h>
-#include "image2d.h"
+#include <image2d.h>
+#include <image3d.h>
 #include <iostream>
 #include <timer.h>
 #include <h5filewriter.h>
@@ -44,17 +45,20 @@ int main(int argc, char *argv[])
   std::vector<std::string> ifnames;
   std::string ofname;
   std::string imageName="stack";
+  bool buffered=false;
   int compression=0;
+  size_t chunksize=32;
   ap.description="tiff to hdf5 stacker for 16-bit single-channel microscopy data";
   ap.bindVector("i",ifnames,"TIFF filenames",true);
-  ap.bind("n",imageName,"image_name","image path in hdf5 file");
   ap.bind("o",ofname,"output_hdf5","output hdf5 file",true);
-  ap.bind("c",compression,"compression_level","level for gzip (0=none; 9=maximum)");
+  ap.bind("n",imageName,"image_name","image path in hdf5 file");
+  ap.bind("c",chunksize,"chunk_size","size of chunks ");
+  ap.bind("z",compression,"compression_level","level for gzip (0=none; 9=maximum)");
+  ap.bindFlag("b",buffered,"buffer writes (faster, uses more memory)");
   if (!ap.parseAndValidate(argc,argv))
   {
     return ap.usage();
   }
-  Image2D image;
   if (argc<2)
   {
     std::cout<<"usage: "<<std::endl;
@@ -72,26 +76,77 @@ int main(int argc, char *argv[])
 
   H5FileWriter h5writer;
   h5writer.open(ofname,firstImageInfo.width,firstImageInfo.height,ifnames.size());
-  h5writer.createImageVar(imageName,compression);
-
-  Timer t0;
-  bool okay=true;
-  for (int i=0;i<ifnames.size();i++)
+  if (!h5writer.createImageVar(imageName,chunksize,compression))
   {
-    Timer t;
-    t.start();
-    Image2D image;
-    if (!image.readTiff(ifnames[i]))
-    {
-      std::cerr<<"unable to read "<<ifnames[i]<<std::endl;
-      okay=false;
-      break;
-    }
-    t.stop();
-    std::cout<<"read "<<ifnames[i]<<" in "<<t.elapsed()<<std::endl;
-    h5writer.writeSlice(image,i);
+    std::cerr<<"could not create image array for /"<<imageName<<". stopping."<<std::endl;
+    return 1;
   }
-  t0.stop();
-  std::cout<<"read tiffs for "<<ifnames.size()<<" images in "<<t0.elapsed()<<std::endl;
+  if (buffered)
+  {
+    Timer t0;
+    bool okay=true;
+    Image3D image;
+    image.initialize(firstImageInfo.width,firstImageInfo.height,chunksize);
+    int bufferPlane=0;
+    int nplanes=ifnames.size();
+    for (size_t z=0;z<nplanes;z+=chunksize)
+    {
+      int readPlanes=0;
+      Timer t0;
+      t0.start();
+      for (size_t i=0;i<chunksize;i++)
+      {
+        if (z+i>=nplanes) break;
+        Timer t;
+        t.start();
+        std::cout<<"reading "<<ifnames[z+i]<<"..."<<std::flush;
+        if (!image.readTiff(ifnames[z+i],i))
+        {
+          std::cerr<<"unable to read "<<ifnames[i]<<std::endl;
+          okay=false;
+          break;
+        }
+        t.stop();
+        std::cout<<"read slice "<<z+i<<" in "<<t.elapsed()<<std::endl;
+        readPlanes++;
+      }
+      if (!okay) break;
+      t0.stop();
+      std::cout<<"read "<<readPlanes<<" tiff slices in "<<t0.elapsed()<<std::endl;
+      if (!h5writer.writeSlab(image,z,readPlanes))
+      {
+        std::cerr<<"error: unable to write to hdf5 file. stopping."<<std::endl;
+        return 1;
+      }
+    }
+    t0.stop();
+    std::cout<<"stacked "<<ifnames.size()<<" tiff slices in "<<t0.elapsed()<<std::endl;
+  }
+  else
+  {
+    Timer t0;
+    bool okay=true;
+    for (int i=0;i<ifnames.size();i++)
+    {
+      Timer t;
+      t.start();
+      Image2D image;
+      if (!image.readTiff(ifnames[i]))
+      {
+        std::cerr<<"unable to read "<<ifnames[i]<<std::endl;
+        okay=false;
+        break;
+      }
+      t.stop();
+      std::cout<<"read "<<ifnames[i]<<" in "<<t.elapsed()<<std::endl;
+      if (!h5writer.writeSlice(image,i))
+      {
+        std::cerr<<"error: unable to write to hdf5 file. stopping."<<std::endl;
+        return 1;
+      }
+    }
+    t0.stop();
+    std::cout<<"read tiffs for "<<ifnames.size()<<" images in "<<t0.elapsed()<<std::endl;
+  }
   return 0;
 }
